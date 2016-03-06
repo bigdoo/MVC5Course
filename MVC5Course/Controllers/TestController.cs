2ec3e85d1cc5f8e557ea736c1f78cde16d99d718
Will <doggy.huang@gmail.com>
2016-03-06 09:32:27 +0800

    13 練習建立一個 MemberViewModel 類別與透過 ViewModel 建立兩個 Action 與相對應的 View 頁面

--- MVC5Course/Controllers/TestController.cs
+++ MVC5Course/Controllers/TestController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MVC5Course.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,6 +10,19 @@ namespace MVC5Course.Controllers
     public class TestController : Controller
     {
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        // Code Snippet Shortcut: mvcaction4
+        public ActionResult MemberProfile()
+        {
+            return View();
+        }
+
+        // Code Snippet Shortcut: mvcpostaction4
+        [HttpPost]
+        public ActionResult MemberProfile(MemberViewModel data)
         {
             return View();
         }
