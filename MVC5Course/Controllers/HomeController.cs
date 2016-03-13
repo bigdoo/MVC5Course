3ab29df7914466a85ebe23cf2539717d32afbfff
Will <doggy.huang@gmail.com>
2016-03-13 12:07:45 +0800

    36 練習使用 Razor 語法控制精準的原始碼輸出 ( int[] data = new int[] { 1, 2, 3, 4, 5 }; ) ( @model Int32[] )
    public ActionResult RazorTest()
    {
        int[] data = new int[] { 1, 2, 3, 4, 5 };
    
        return PartialView(data);
    }
    
    @model int[]
    
    @Model

--- MVC5Course/Controllers/HomeController.cs
+++ MVC5Course/Controllers/HomeController.cs
@@ -50,5 +50,12 @@ namespace MVC5Course.Controllers
 
             return Content("No Error");
         }
+
+        public ActionResult RazorTest()
+        {
+            int[] data = new int[] { 1, 2, 3, 4, 5 };
+
+            return PartialView(data);
+        }
     }
 }
